c02acd5770487de77335840bded891359ca795dd
wilsjame <jgwilson1214@gmail.com>
2020-12-30 16:55:47 -0800

    Day 20: Jurassic Jigsaw part 1

--- 2020/20/Day20-1-Jurassic-Jigsaw.cpp
+++ 2020/20/Day20-1-Jurassic-Jigsaw.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 #define MAXN 10 
+#define ll long long
 #define print(x) cerr << #x << " is " << x << endl;
 
 typedef struct Board Board;
 struct Board {
     int ID;
     char b[MAXN][MAXN];
-
 };
 
-// Rotate 90 degree clockwise: [r, c] -> [c, n - 1 - r]
+// rotate 90 degree clockwise: [r, c] -> [c, n - 1 - r]
 Board rotate(Board b) {
     Board nb;
     
@@ -24,7 +25,7 @@ Board rotate(Board b) {
     return nb;
 }
 
-// Reflect board horizontally: [r, c] -> [r, n - 1 - c]
+// reflect board horizontally: [r, c] -> [r, n - 1 - c]
 Board reflect(Board b) {
     Board nb;
 
@@ -33,6 +34,51 @@ Board reflect(Board b) {
         for (int c = 0; c < MAXN; c++) 
             nb.b[r][MAXN - 1 - c] = b.b[r][c];
     return nb;
+}
+
+// return true if and only if boards match a side
+bool matchN(Board b, Board bb) {
+    vector<char> bN, bbS;
+    for (int k = 0; k < MAXN; k++) 
+        bN.push_back(b.b[0][k]);
+    for (int k = 0; k < MAXN; k++) 
+        bbS.push_back(bb.b[MAXN - 1][k]);
+    return bN == bbS;
+}
+bool matchE(Board b, Board bb) {
+    vector<char> bE, bbW;
+    for (int k = 0; k < MAXN; k++) 
+        bE.push_back(b.b[k][MAXN - 1]);
+    for (int k = 0; k < MAXN; k++) 
+        bbW.push_back(bb.b[k][0]);
+    return bE == bbW;
+}
+bool matchS(Board b, Board bb) {
+    vector<char> bS, bbN;
+    for (int k = 0; k < MAXN; k++) 
+        bS.push_back(b.b[MAXN - 1][k]);
+    for (int k = 0; k < MAXN; k++) 
+        bbN.push_back(bb.b[0][k]);
+    return bS == bbN;
+}
+bool matchW(Board b, Board bb) {
+    vector<char> bW, bbE;
+    for (int k = 0; k < MAXN; k++) 
+        bW.push_back(b.b[k][0]);
+    for (int k = 0; k < MAXN; k++) 
+        bbE.push_back(bb.b[k][MAXN - 1]);
+    return bW == bbE;
+
+}
+
+// return non 'X' if and only if boards match any side
+char matchA(Board b, Board bb) {
+    char res = 'X';
+    if (matchN(b, bb)) res = 'N'; 
+    else if (matchE(b, bb)) res = 'E';
+    else if (matchS(b, bb)) res = 'S';
+    else if (matchW(b, bb)) res = 'W';
+    return res;
 }
 
 Board rdboard(int n) {
@@ -69,15 +115,28 @@ int main() {
             A.push_back(b);
         }
     }
+    int N = sqrt(A.size());
 
-    // debug
-    for (Board b : A) {
-        printb(b);
-        cerr << "rotated!" << endl;
-        printb(rotate(b));
-        cerr << "and reflected!" << endl;
-        printb(reflect(rotate(b)));
+    ll ans = 1;
+    for (int t = 0; t < N * N; t++) {
+        int mc = 0;
+        Board b = A[t];
+        for (int tt = 0; tt < N * N; tt++) {
+            if (tt == t) continue;
+            Board bb = A[tt];
+            for (int i = 0; i < 4; i++) {
+                mc += matchA(b, bb) != 'X';
+                bb = rotate(bb);
+            }
+            bb = reflect(bb);
+            for (int i = 0; i < 4; i++) {
+                mc += matchA(b, bb) != 'X';
+                bb = rotate(bb);
+            }
+        }
+        if (mc == 2) ans *= b.ID;
     }
+    print(ans);
 
     return 0;
 }

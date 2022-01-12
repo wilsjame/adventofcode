b3fc80323793eaf71dce3f09994dc0888898ca4d
James Wilson <jgwilson1214@gmail.com>
2022-01-11 19:30:56 -0800

    day 10: syntax scoring

--- 2021/day10.py
+++ 2021/day10.py
@@ -1,7 +1,7 @@
 data: list[str] = []
 with open('input/day10.txt') as f:
     for line in f:
-        data.append(line)
+        data.append(line.strip())
 
 # Part One
 def match(l: str, r: str) -> bool:
@@ -13,16 +13,59 @@ def match(l: str, r: str) -> bool:
 ans:   int = 0
 stack: list[str] = []
 score: dict[str, int] = {')': 3, ']': 57, '}': 1197, '>': 25137}
+incomplete: list[str] = []
 for line in data:
+    part2 = True
     for x in line:
         if x == ')' or x == ']' or x == '}' or x == '>':
             top = stack.pop(0)
             if match(top, x) == True:
                 continue
             else:
-                #print('invalid', x)
                 ans += score[x]
+                part2 = False
                 break
         else:
             stack.insert(0, x)
+    if part2 == True:
+        incomplete.append(line)
 print(ans)
+
+# Part Two
+def pair(l: str) -> str:
+    ret: str
+    if l == '(':
+        ret = ')' 
+    elif l == '[':
+        ret = ']'
+    elif l == '{':
+        ret = '}'
+    elif l == '<':
+        ret = '>'
+    return ret
+
+def calc_score(a: list[str], score: dict[str, int]) -> int:
+    res = 0
+    for x in a:
+        res *= 5
+        res += score[pair(x)]
+    return res
+
+points: list[int] = []
+score = {')': 1, ']': 2, '}': 3, '>': 4}
+for line in incomplete:
+    stack = []
+    for x in line:
+        if x == ')' or x == ']' or x == '}' or x == '>':
+            top = stack.pop(0)
+            if match(top, x) == True:
+                continue
+            else:
+                ans += score[x]
+                part2 = False
+                break
+        else:
+            stack.insert(0, x)
+    points.append(calc_score(stack, score))
+    points.sort()
+print(points[len(points)//2])

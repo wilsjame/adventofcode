e19a1418386596aabd2d41d6edfb2f99bfaa7d2a
James Wilson <jgwilson1214@gmail.com>
2022-03-13 22:27:49 -0700

    day 22 part 2 🐢⚰️

--- 2021/day22.py
+++ 2021/day22.py
@@ -13,6 +13,9 @@ with open('input/day22.txt') as f:
             step.append([int(k) for k in ranges.strip(',yz').split('..')])
         steps.append(step)
 
+
+# Part One
+
 def inrange(x: Range, y: Range, z: Range) -> bool:
     if x[0] < -50 or y[0] < -50 or y[0] < -50 or x[1] > 50 or x[1] > 50 or z[1] > 50:
         return False
@@ -39,3 +42,67 @@ cnt: int = 0
 for k,v in vol.items():
     cnt += v == True
 print(cnt)
+
+# Part Two coordinate compression
+
+# this approach uses a ton of memory >10GB rip
+# crashes cpython, atleast on my pc
+# works using pypy, but will crash, sometimes ;_;
+
+# store unique x, y, and z coordinates
+# map coordinates to compressed indices
+# map compressed indices to coordinates
+# store compressed 'on' cubes
+# expand compressed 'on' cubes
+
+X: set[int] = set()
+Y: set[int] = set()
+Z: set[int] = set()
+for s in steps:
+    X.update([s[1][0], s[1][1] + 1]) # add one to include upper bound
+    Y.update([s[2][0], s[2][1] + 1]) # ex) 1..2 is two elements 
+    Z.update([s[3][0], s[3][1] + 1]) #     (2 + 1) - 1 = 2
+X = sorted(X)
+Y = sorted(Y)
+Z = sorted(Z)
+
+Xx: dict[int, int] = {}
+Yy: dict[int, int] = {}
+Zz: dict[int, int] = {}
+for i, v in enumerate(X):
+    Xx[v] = i
+for i, v in enumerate(Y):
+    Yy[v] = i
+for i, v in enumerate(Z):
+    Zz[v] = i
+xX: dict[int, int] = {}
+yY: dict[int, int] = {}
+zZ: dict[int, int] = {}
+for k, v in Xx.items():
+    xX[v] = k
+for k, v in Yy.items():
+    yY[v] = k
+for k, v in Zz.items():
+    zZ[v] = k
+
+V: set[Pt] = set() # compressed 'on' cubes
+for i, s in enumerate(steps):
+    #print(i+1, len(steps))
+    x0: int = Xx[s[1][0]] 
+    x1: int = Xx[s[1][1] + 1] 
+    y0: int = Yy[s[2][0]]
+    y1: int = Yy[s[2][1] + 1]
+    z0: int = Zz[s[3][0]]
+    z1: int = Zz[s[3][1] + 1]
+    for i in range(x0, x1):
+        for j in range(y0, y1):
+            for k in range(z0, z1):
+                if s[0] == 'on':
+                    V.add((i, j, k))
+                else:
+                    V.discard((i, j, k))
+
+ans = 0
+for i, j, k in V:
+    ans += (xX[i+1] - xX[i]) * (yY[j+1] - yY[j]) * (zZ[k+1] - zZ[k])
+print(ans)

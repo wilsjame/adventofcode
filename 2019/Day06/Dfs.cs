deefe70799e28f50238f7fb833c797dd9b0f219e
James Wilson <jgwilson1214@gmail.com>
2022-05-12 15:02:47 -0700

    day 6

---
+++ 2019/Day06/Dfs.cs
@@ -0,0 +1,31 @@
+﻿namespace day06;
+
+public class Dfs
+{
+    private Dictionary<string, List<string>> adj;
+
+    public Dfs(Dictionary<string, List<string>> graph)
+    {
+        adj = graph;
+    }
+    
+    // Returns the path from s to "COM" in the adj graph.
+    public List<string> Search(string s, List<string> path)
+    {
+        
+        if (s == "COM")
+        {
+            return path;
+        }
+
+        foreach (string u in adj[s])
+        {
+            path.Add(u);
+            Search(u, path);
+        }
+
+        return path;
+    }
+    
+}
+
---
+++ 2019/Day06/Program.cs
@@ -0,0 +1,61 @@
+﻿using day06;
+    
+List<string> lines = File.ReadAllLines("day06.in").ToList();
+
+Dictionary<string, List<string>> adj = new();
+HashSet<string> myset = new();
+
+foreach (string line in lines)
+{
+    string u = line.Split(')')[0];
+    string v = line.Split(')')[1];
+    
+    if (adj.ContainsKey(v))
+    {
+        adj[v].Add(u);
+    }
+    else
+    {
+        adj[v] = new List<string> { u };
+    }
+
+    myset.Add(u);
+    myset.Add(v);
+}
+
+Dfs dfs = new Dfs(adj);
+
+// Part 1.
+
+int ans1 = 0;
+
+foreach (string u in myset)
+{
+    List<string> path = dfs.Search(u, new List<string>());
+    ans1 += path.Count;
+}
+
+Console.WriteLine(ans1);
+
+// Part 2.
+
+List<string> path1 = dfs.Search("YOU", new List<string>());
+List<string> path2 = dfs.Search("SAN", new List<string>());
+string meet = null;
+
+foreach (string u in path1)
+{
+    
+    if (path2.Contains(u))
+    {
+        // Where path1 and path2 meet.
+        meet = u;
+        break;
+    }
+    
+}
+
+int d1 = path1.IndexOf(meet);
+int d2 = path2.IndexOf(meet);
+int ans2 = d1 + d2;
+Console.WriteLine(ans2);

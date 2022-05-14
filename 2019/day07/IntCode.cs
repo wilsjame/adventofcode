490158c3fe1c6c7755c9a4da066428f77e1fa832
James Wilson <jgwilson1214@gmail.com>
2022-05-14 15:11:44 -0700

    day 7 part 1 (a little al dente 🍝)

---
+++ 2019/day07/IntCode.cs
@@ -0,0 +1,142 @@
+﻿namespace day07;
+
+public class IntCode
+{
+    private List<int> Arr;
+    private Queue<int> Q = new();
+
+    public IntCode(List<int> arr, int a, int b)
+    {
+        Arr = arr;
+        Q.Enqueue(a);
+        Q.Enqueue(b);
+    }
+
+    public int Run()
+    {
+        int ptr = 0;
+        
+        while (true)
+        {
+            
+            // ABCDE where DE - opcode, C - mode of 1st parameter, B - mode of 2nd, A - 3rd
+            //  1002                    0 is position mode and 1 is immediate mode.
+            string op = Arr[ptr].ToString().PadLeft(5, '0');
+            int code = int.Parse(op.Substring(3));
+            int m1 = int.Parse(op.Substring(2, 1));
+            int m2 = int.Parse(op.Substring(1, 1));
+            int m3 = int.Parse(op.Substring(0, 1));
+
+            // code 99 - halts,  1 - *, 2 - +, 3 - input, 4 - outputs
+            // 5 - jump-if-true, 6 - jump-if-false, 7 - less than, 8 - equals
+            // TODO refactor with a switch statement if we add more opcodes.
+            if (code == 99)
+            {
+                break;
+            }
+            else if (code == 1 || code == 2)
+            {
+                
+                // Set values based on parameter modes.
+                int v1 = (m1 == 0) ? Arr[Arr[ptr + 1]] : Arr[ptr + 1];
+                int v2 = (m2 == 0) ? Arr[Arr[ptr + 2]] : Arr[ptr + 2];
+                int pos = Arr[ptr + 3];
+
+                if (code == 1)
+                {
+                    Arr[pos] = v1 + v2;
+                }
+                else if (code == 2)
+                {
+                    Arr[pos] = v1 * v2;
+                }
+
+                ptr += 4;
+            }
+            else if (code == 3 || code == 4)
+            {
+                int pos = Arr[ptr + 1];
+
+                if (code == 3)
+                {
+                    Arr[pos] = Q.Dequeue();
+                }
+                else if (code == 4)
+                {
+                    //Console.WriteLine($"output at addr {pos} {Arr[pos]}");
+                    return Arr[pos];
+                }
+
+                ptr += 2;
+            }
+            else if (code > 4)
+            {
+                
+                // Set values based on parameter modes.
+                int v1 = (m1 == 0) ? Arr[Arr[ptr + 1]] : Arr[ptr + 1];
+                int v2 = (m2 == 0) ? Arr[Arr[ptr + 2]] : Arr[ptr + 2];
+                int pos = Arr[ptr + 3];
+
+                if (code == 5)
+                {
+                    
+                    if (v1 > 0)
+                    {
+                        ptr = v2;
+                    }
+                    else
+                    {
+                        ptr += 3;
+                    }
+                    
+                }
+                else if (code == 6)
+                {
+                    
+                    if (v1 == 0)
+                    {
+                        ptr = v2;
+                    }
+                    else
+                    {
+                        ptr += 3;
+                    }
+                    
+                }
+                else if (code == 7)
+                {
+
+                    if (v1 < v2)
+                    {
+                        Arr[pos] = 1;
+                    }
+                    else
+                    {
+                        Arr[pos] = 0;
+                    }
+
+                    ptr += 4;
+                }
+                else if (code == 8)
+                {
+
+                    if (v1 == v2)
+                    {
+                        Arr[pos] = 1;
+                    }
+                    else
+                    {
+                        Arr[pos] = 0;
+                    }
+
+                    ptr += 4;
+                }
+                
+            }
+            
+        }
+
+        return 0;
+    }
+    
+}
---
+++ 2019/day07/Program.cs
@@ -0,0 +1,69 @@
+﻿using day07;
+
+List<int> arr = File.ReadAllLines("day07.in")[0].Split(",").Select(x => int.Parse(x)).ToList();
+
+// Part 1.
+
+int mx = 0;
+
+// Variables used by search().
+List<bool> chosen = new List<bool>();
+
+int n = 5;
+for (int i = 0; i < n; i++)
+{
+    chosen.Add(false);
+}
+
+List<int> permutation = new List<int>();
+
+// search() generates all permutations of a number of length n
+// consisting of digits [0, n). 
+void search()
+{
+    
+    if (permutation.Count == n)
+    {
+        // process permutation
+        Stack<int> st = new Stack<int>(permutation);
+        int last_out = 0;
+        
+        while (st.Count > 0)
+        {
+            int aa = st.Pop();
+            int bb = last_out;
+            
+            IntCode bot = new IntCode(new List<int>(arr), aa, bb);
+            last_out = bot.Run();
+        }
+        
+        if (last_out > mx)
+        {
+            mx = last_out;
+        }
+                            
+    }
+    else
+    {
+        
+        for (int i = 0; i < n; i++)
+        {
+            
+            if (chosen[i] == true)
+            {
+                continue;
+            }
+            
+            chosen[i] = true;
+            permutation.Add(i);
+            search();
+            chosen[i] = false;
+            permutation.RemoveAt(permutation.Count - 1);
+        }
+        
+    }
+    
+}
+
+search();
+Console.WriteLine($"mx {mx}");
